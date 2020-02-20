1fffb1c7cb34e9cdadcb2dc8b9ca3c9c1e4b32b8
Shadow <42289385+sadeghgaga@users.noreply.github.com>
2020-02-20 14:26:19 +0330

    webview for guide

--- app/src/main/java/net/danteh/dantehviewer/fragments/GuideFragment.java
+++ app/src/main/java/net/danteh/dantehviewer/fragments/GuideFragment.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import net.danteh.dantehviewer.R;
 
@@ -15,7 +17,7 @@ import net.danteh.dantehviewer.R;
  * A simple {@link Fragment} subclass.
  */
 public class GuideFragment extends Fragment {
-
+    WebView webView;
 
     public GuideFragment() {
         // Required empty public constructor
@@ -28,6 +30,14 @@ public class GuideFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_guide, container, false);
 
+        webView = v.findViewById(R.id.wv);
+
+        WebViewClient client = new WebViewClient();
+        webView.setWebViewClient(client);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.loadUrl("file:///android_asset/index.html");
         return v;
     }
 
